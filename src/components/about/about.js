bb2d45078424670df6ebcbefa47ac3987734a0af
Sofya <sonya.konovalova.04@gmail.com>
2026-05-25 12:46:54 +0500

    новые секции на страницу о нас

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react'
 import './about.css'
 
 export default class about extends Component {
+
+  maskot = [
+    {
+      name: "Радость",
+      info: "текст о радости",
+      img: 'images/happy.png',
+    },
+    {
+      name: "Тревожность",
+      info: "текст о тревожности",
+      img: 'images/worry.png',
+    },
+    {
+      name: "Страх",
+      info: "текст о страхе",
+      img: 'images/worry.png',
+    }
+  ]
+
+  state = {
+    current: 0
+  }
+
+  prev = () => {
+    this.setState(s => ({
+      current: (s.current - 1 + this.maskot.length) % this.maskot.length
+    }))
+  }
+
+  next = () => {
+    this.setState(s => ({
+      current: (s.current + 1) % this.maskot.length
+    }))
+  }
+
   render() {
     return (
       <div className='about'>
@@ -18,11 +53,145 @@ export default class about extends Component {
         </div>
 
         <div className='address-card'>
-            <div className='adress'></div>
-            <div className='adress'></div>
-            <div className='adress'></div>
+            <div className='adress'>
+              <div className='map'>
+                <div className='map-wrapper'>
+                  <a href="https://yandex.ru/maps/54/yekaterinburg/?utm_medium=mapframe&utm_source=maps">Екатеринбург</a>
+                  <a href="https://yandex.ru/maps/54/yekaterinburg/house/ulitsa_malysheva_51/YkkYcAZkT0AHQFtsfXRyd31jYg==/?indoorLevel=1&ll=60.614755%2C56.836161&utm_medium=mapframe&utm_source=maps&z=18.15">Улица Малышева, 51 — Яндекс Карты</a>
+                  <iframe
+                    src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=60.614755%2C56.836161&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjA5NzExMRJw0KDQvtGB0YHQuNGPLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsINCV0LrQsNGC0LXRgNC40L3QsdGD0YDQsywg0YPQu9C40YbQsCDQnNCw0LvRi9GI0LXQstCwLCA1MSIKDWd1ckIVLVhjQg%2C%2C&z=18.15"
+                    width="560"
+                    height="400"
+                    allowFullScreen={true}
+                    className='map-iframe'
+                    title="Яндекс Карты"
+                  ></iframe>
+                </div>
+              </div>
+              <p>Конкретный адресс</p>
+            </div>
+            <div className='adress'>
+              <div className='map'>
+                  <div className='map-wrapper'>
+                  <a href="https://yandex.ru/maps/54/yekaterinburg/?utm_medium=mapframe&utm_source=maps">Екатеринбург</a>
+                  <a href="https://yandex.ru/maps/54/yekaterinburg/house/ulitsa_malysheva_51/YkkYcAZkT0AHQFtsfXRyd31jYg==/?indoorLevel=1&ll=60.614755%2C56.836161&utm_medium=mapframe&utm_source=maps&z=18.15">Улица Малышева, 51 — Яндекс Карты</a>
+                  <iframe
+                    src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=60.614755%2C56.836161&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjA5NzExMRJw0KDQvtGB0YHQuNGPLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsINCV0LrQsNGC0LXRgNC40L3QsdGD0YDQsywg0YPQu9C40YbQsCDQnNCw0LvRi9GI0LXQstCwLCA1MSIKDWd1ckIVLVhjQg%2C%2C&z=18.15"
+                    width="560"
+                    height="400"
+                    allowFullScreen={true}
+                    className='map-iframe'
+                    title="Яндекс Карты"
+                  ></iframe>
+                </div>
+              </div>
+              <p>Конкретный адресс</p>
+            </div>
+            <div className='adress'>
+              <div className='map'>
+                <div className='map-wrapper'>
+                  <a href="https://yandex.ru/maps/54/yekaterinburg/?utm_medium=mapframe&utm_source=maps">Екатеринбург</a>
+                  <a href="https://yandex.ru/maps/54/yekaterinburg/house/ulitsa_malysheva_51/YkkYcAZkT0AHQFtsfXRyd31jYg==/?indoorLevel=1&ll=60.614755%2C56.836161&utm_medium=mapframe&utm_source=maps&z=18.15">Улица Малышева, 51 — Яндекс Карты</a>
+                  <iframe
+                    src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=60.614755%2C56.836161&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjA5NzExMRJw0KDQvtGB0YHQuNGPLCDQodCy0LXRgNC00LvQvtCy0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsINCV0LrQsNGC0LXRgNC40L3QsdGD0YDQsywg0YPQu9C40YbQsCDQnNCw0LvRi9GI0LXQstCwLCA1MSIKDWd1ckIVLVhjQg%2C%2C&z=18.15"
+                    width="560"
+                    height="400"
+                    allowFullScreen={true}
+                    className='map-iframe'
+                    title="Яндекс Карты"
+                  ></iframe>
+                </div>
+              </div>
+              <p>Конкретный адресс</p>
+            </div>
         </div>
 
+      <div className='maskot'>
+        <h2>Кто такой Маскот</h2>
+        <div>
+          <img src='#' alt='Искра маскот'></img>
+        </div>
+      </div>
+
+
+      <div className='pluses'>
+        <h2>Преимущества</h2>
+        <p className='pluses-subtitle'>Какой-то текст</p>
+        <div className='pluses-grid'>
+          <div className='pluses-card'>
+            <p>Заголовок</p>
+            <p>Какой-то текст</p>
+            <img src='#' alt='преимущстево'></img>
+          </div>
+
+          <div className='pluses-card'>
+            <p>Заголовок</p>
+            <p>Какой-то текст</p>
+            <img src='#' alt='преимущстево'></img>
+          </div>
+
+          <div className='pluses-card'>
+            <p>Заголовок</p>
+            <p>Какой-то текст</p>
+            <img src='#' alt='преимущстево'></img>
+          </div>
+
+          <div className='pluses-card'>
+            <p>Заголовок</p>
+            <p>Какой-то текст</p>
+            <img src='#' alt='преимущстево'></img>
+          </div>
+
+          <div className='pluses-card'>
+            <p>Заголовок</p>
+            <p>Какой-то текст</p>
+            <img src='#' alt='преимущстево'></img>
+          </div>
+
+          <div className='pluses-card'>
+            <p>Заголовок</p>
+            <p>Какой-то текст</p>
+            <img src='#' alt='преимущстево'></img>
+          </div>
+        </div>
+      </div>
+
+      <div className='about-form'>
+        <p>Приглашаем на первую встречу — чтобы познакомиться и понять, подходим ли мы вам.</p>
+        <div className='form'>
+        <textarea placeholder='Номер телефона'></textarea>
+          <div>
+            <input type="checkbox" id="policy" name="poilcy" checked />
+            <label for="scales">Согласие на обработку персональных данных</label>
+          </div>
+          <button type='submit'>оставить заявку</button>
+        </div>
+      </div>
+
+
+      <div className='kartochki'>
+        <h2>Карточки</h2>
+        <div className='kartochki-track'>
+          {[0, 1].map(offset => {
+            const item = this.maskot[(this.state.current + offset) % this.maskot.length]
+            return (
+              <div className='kartochki-card' key={offset}>
+                <h3>{item.name}</h3>
+                <p>{item.info}</p>
+                <img src={item.img} alt={item.name} />
+                <div className='button'>
+                  <button className='kartochki-btn'>читать подробнее</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className='kartochki-arrows'>
+          <button className='kartochki-arrow' onClick={this.prev}><img src='images/left_arrow.svg' alt='назад'></img></button>
+          <button className='kartochki-arrow' onClick={this.next}><img src='images/right_arrow.svg' alt='вперёд'></img></button>
+        </div>
+      </div>
+    
 
       </div>
     )
--- src/components/place/place.js
+++ src/components/place/place.js
@@ -2,19 +2,55 @@ import React, { Component } from 'react'
 import './place.css'
 
 export default class place extends Component {
+
+  carusel = [
+    { title: 'Адрес 1', img: 'images/photo1.png' },
+    { title: 'Адрес 2', img: 'images/photo2.png' },
+    { title: 'Адрес 3', img: 'images/photo3.png' },
+  ]
+
+  state = {
+    current: 0
+  }
+
+  prev = () => {
+    this.setState(s => ({
+      current: (s.current - 1 + this.carusel.length) % this.carusel.length
+    }))
+  }
+
+  next = () => {
+    this.setState(s => ({
+      current: (s.current + 1) % this.carusel.length
+    }))
+  }
+
   render() {
+    const { current } = this.state
+    const len = this.carusel.length
+    const prevIdx = (current - 1 + len) % len
+    const nextIdx = (current + 1) % len
+
     return (
       <div className='place'>
         <h2>В центре уютно и спокойно</h2>
         <p className='place-text'>Наши кабинеты — это пространство, где можно быть собой и меняться в своем темпе</p>
-        <div className='place-img'>
-            <div>
-                <img src='images/photo1.png' alt='фото 1'></img>
-            </div>
-            <div>
-                <img src='images/photo2.png' alt='фото 2'></img>
-                <img src='images/photo3.png' alt='фото 3'></img>
-            </div>
+
+        <div className='carousel'>
+
+          <div className='carousel-side' onClick={this.prev}>
+            <img src={this.carusel[prevIdx].img} alt='предыдущий' />
+          </div>
+
+          <div className='carousel-center'>
+            <img src={this.carusel[current].img} alt={this.carusel[current].title} />
+            <p className='carousel-title'>{this.carusel[current].title}</p>
+          </div>
+
+          <div className='carousel-side' onClick={this.next}>
+            <img src={this.carusel[nextIdx].img} alt='следующий' />
+          </div>
+
         </div>
       </div>
     )
